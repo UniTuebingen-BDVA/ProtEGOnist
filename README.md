83fffcde859efe9f9127bc4e8bd667c2efb8f5ff
MarcoSchaeferT <marco.schaefer@uni-tuebingen.de>
2023-06-21 17:55:18 +0200

    improved getData() + extended readme

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # BiovisChallenge2023
 
-A Folder /data/ is needed in which to put the respective data file
+(A Folder /data/ is needed in which to put the respective data file) ### should be automatically created
 
 # Setup
 
 conda env create -f environment.yml
+
+conda activate biovischallenge
+
+cd python
+
+python heatmap.py
+
+# VS Code
+
+add "Python Environment Manager" --> to help Pylance to use the correct environment
--- python/heatmap.py
+++ python/heatmap.py
@@ -7,42 +7,51 @@ import os
 
 
 cwdPath: Path = Path(os.getcwd())
-dataTargetpath = cwdPath / "ProCan-DepMapSanger_protein_matrix_8498_averaged.txt"
+dataPath = Path(
+    cwdPath.parent / "data" / "ProCan-DepMapSanger_protein_matrix_8498_averaged.txt"
+)
+dataURL = "https://bdvanc.uni-tuebingen.de/index.php/s/BerA8DoTy8EEPsM/download/ProCan-DepMapSanger_protein_matrix_8498_averaged.txt"
 
 
 def getData():
-    dataSource = "https://bdvanc.uni-tuebingen.de/index.php/s/BerA8DoTy8EEPsM/download/ProCan-DepMapSanger_protein_matrix_8498_averaged.txt"
-    if os.path.exists(dataTargetpath):
-        print("data already dwonloaded")
+    dataTargetPath: Path = dataPath
+
+    if not dataTargetPath.parent.exists():
+        os.mkdir(dataTargetPath.parent)
+    if dataTargetPath.exists():
+        print("data already downloaded")
         return
-    print("getData" + str(dataTargetpath))
+    print("getData" + str(dataTargetPath))
     try:
-        wget.download(dataSource, str(dataTargetpath))
+        wget.download(dataURL, str(dataTargetPath))
     except OSError:
-        print("FAILED: to Download: " + str(dataTargetpath))
+        print("FAILED to download: " + str(dataTargetPath))
+
+
+def showHeatmap():
+    # Read the data
+    df: pd.DataFrame = pd.read_csv(
+        str(dataPath), sep="\t", index_col=0, header=0, engine="python"
+    )
+    print(df.shape)
+    df = df.dropna(axis=1, how="all")
+    print(df.shape)
+    df.fillna(0, inplace=True)
+
+    print(df.head(5))
+
+    cg = sns.clustermap(
+        df,
+        cmap="RdBu_r",
+        col_cluster=False,
+        center=0,
+        figsize=(20, 20),
+        vmin=-5,
+        vmax=5,
+        z_score=1,
+    )
+    plt.show()
 
 
 getData()
-dataPath: Path = dataTargetpath
-# Read the data
-df: pd.DataFrame = pd.read_csv(
-    str(dataPath), sep="\t", index_col=0, header=0, engine="python"
-)
-print(df.shape)
-df = df.dropna(axis=1, how="all")
-print(df.shape)
-df.fillna(0, inplace=True)
-
-print(df.head(5))
-
-cg = sns.clustermap(
-    df,
-    cmap="RdBu_r",
-    col_cluster=False,
-    center=0,
-    figsize=(20, 20),
-    vmin=-5,
-    vmax=5,
-    z_score=1,
-)
-plt.show()
+showHeatmap()
