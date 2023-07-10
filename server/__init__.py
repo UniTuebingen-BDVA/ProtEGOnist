7ffb47e460a3866129b2e03fb9684e796044420e
ThHarbig <theresa_ha@hotmail.de>
2023-07-10 09:33:11 +0200

    add index.html route to flask for deployment

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='../dist', static_url_path='/')
 here = os.path.dirname(__file__)
 
 
@@ -10,3 +10,8 @@ here = os.path.dirname(__file__)
 def test():
     counter = int(request.form.to_dict()['counter'])
     return str(counter+1)
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
