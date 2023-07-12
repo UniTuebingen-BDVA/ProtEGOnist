7e96d6095eb086d8cafb7e6561b4849b0df618aa
NiBrc <26893899+NiBrc@users.noreply.github.com>
2023-07-12 14:10:36 +0200

    Tested Basic EgoGraph structure

---
+++ server/python_scripts/egoGraph.py
@@ -0,0 +1,120 @@
+import networkx as nx
+import json
+
+class Intersection():
+  intersection: list[str|int]
+  len1_prop: float
+  len2_prop: float
+  len3_prop: float
+  len4_prop: float
+
+class egoGraph():
+    """
+    A class representing the ego graph of a given node.
+    
+    Attributes:
+        node: The node that is the center of the ego graph.
+        graph: The networkx ego-graph object of the ego graph.
+    
+    Methods:
+        __init__(self, node, graph): Constructor.
+        getGraphJSON(self): Returns the graph object as a JSON representation.
+        getNodes(self): Returns all nodes in the graph.
+        getEdges(self): Returns all edges in the graph.
+        getNeighbors(self): Returns all degree-1 and degree2 neighbors of the node.
+        getIntersection(self, target): Returns the intersection of the ego graph and the target ego graph.
+    """
+    def __init__(self, node: str|int, graph: nx.Graph):
+      """
+      Constructor.
+      
+      Parameters:
+        node: The node that is the center of the ego graph.
+        graph: The networkx graph object of the ego graph.
+      """
+      self.node = node
+      self.nxGraph: nx.Graph = nx.ego_graph(graph, node, radius=2)
+
+    def getGraphJSON(self) -> str:
+      """
+      Returns the graph object as a serialized JSON representation.
+      TODO: Currently this returns all attributes of nodes and edges. This might not be necessary.
+      
+      Returns:
+        A JSON representation of the graph object.
+      """
+      return json.dumps(nx.node_link_data(self.nxGraph, {"source":"from", "target":"to","link":"edges"}))
+    
+    
+    def getNeighbors(self) -> dict[str, list[str|int]]:
+      """
+      Returns all degree-1 and degree2 neighbors of the node.
+      
+      Returns:
+        A list of all degree-1 and degree2 neighbors of the node.
+      """
+      try:
+        t1_neighbors: list[str|int] = list(self.nxGraph.neighbors(self.node))
+        t2_neighbors: list[str|int] = list(nx.non_neighbors(self.nxGraph, self.node))
+      except nx.NetworkXError:
+        t1_neighbors = []
+        t2_neighbors = []
+        print("Node not in graph")
+      return {"t1_neighbors": t1_neighbors, "t2_neighbors": t2_neighbors}
+    
+    def getIntersection(self, target: "egoGraph") -> Intersection:
+      """
+      Returns the intersection of the ego graph and the target ego graph.
+      As both graphs are undirected, the intersection is symmetric.
+      As two levels of neighbors are considered the intersections correspond to paths of different lengths.
+      This means in cases where one of the ego-nodes is a direct neighbor of the other, the length of this element is 1.
+      If direct neighbors of the ego-nodes are identical, the length of this element is 2.
+      If a degree-2 neighbor of one ego-node is a direct neighbor of the other, the length of this element is 3.
+      If a degree-2 neighbor of one ego-node is a degree-2 neighbor of the other, the length of this element is 4.
+      This function reports the intersection set and the proportions of paths of different lengths.
+      
+      Parameters:
+        target: The target ego graph.
+      
+      Returns:
+        A dictionary containing the intersection set and the proportions of paths of different lengths.
+      """
+      self_neighbors = self.getNeighbors()
+      target_neighbors = target.getNeighbors()
+      print("ABC",target_neighbors)
+      # union of t1 and t2 neighbors
+      all_self = self_neighbors["t1_neighbors"] + self_neighbors["t2_neighbors"] + [self.node]
+      all_target = target_neighbors["t1_neighbors"] + target_neighbors["t2_neighbors"] + [target.node]
+      
+      # all intersections
+      intersection = list(set(all_self).intersection(set(all_target)))
+      # Intersection elements of length 1
+      len1_intersection_self_target = list(set([self.node]).intersection(set(target_neighbors["t1_neighbors"])))
+      len1_intersection_target_self = list(set([target.node]).intersection(set(self_neighbors["t1_neighbors"])))
+      # Intersection elements of length 2
+      len2_intersection = list(set(self_neighbors["t1_neighbors"]).intersection(set(target_neighbors["t1_neighbors"])))
+      len2_intersection_self_target = list(set([self.node]).intersection(set(target_neighbors["t2_neighbors"])))
+      len2_intersection_target_self = list(set([target.node]).intersection(set(self_neighbors["t2_neighbors"])))
+      # Intersection elements of length 3
+      len3_intersection_self_target = list(set(self_neighbors["t1_neighbors"]).intersection(set(target_neighbors["t2_neighbors"])))
+      len3_intersection_target_self = list(set(self_neighbors["t2_neighbors"]).intersection(set(target_neighbors["t1_neighbors"])))
+      # Intersection elements of length 4
+      len4_intersection = list(set(self_neighbors["t2_neighbors"]).intersection(set(target_neighbors["t2_neighbors"])))
+
+      # Proportions of paths of different lengths
+      len1_prop = len(len1_intersection_self_target) + len(len1_intersection_target_self)
+      len2_prop = len(len2_intersection) + len(len2_intersection_self_target) + len(len2_intersection_target_self)
+      len3_prop = len(len3_intersection_self_target) + len(len3_intersection_target_self)
+      len4_prop = len(len4_intersection)
+      total_prop = len1_prop + len2_prop + len3_prop + len4_prop
+      print(total_prop == len(intersection))
+      len1_prop = len1_prop / total_prop
+      len2_prop = len2_prop / total_prop
+      len3_prop = len3_prop / total_prop
+      len4_prop = len4_prop / total_prop
+
+      return {"intersection": intersection, "len1_prop": len1_prop, "len2_prop": len2_prop, "len3_prop": len3_prop, "len4_prop": len4_prop}
+    
+
+
+    
---
+++ server/tests/test_egoGraph.py
@@ -0,0 +1,79 @@
+import unittest
+import networkx as nx
+import pytest
+from server.python_scripts.egoGraph import egoGraph
+
+@pytest.fixture(scope="module")
+def G():
+    """
+    A fixture that returns a networkx graph.
+    """
+    G = nx.Graph()
+    G.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1), (2, 7), (3, 8), (4, 9), (5, 10), (6, 11), (1, 12)])
+    yield G
+
+class TestConstructor:
+    """
+    Test the constructor of the egoGraph class.
+    """
+    def test_constructor(self, G):
+        # print type of G
+        print(type(G))
+
+        # Create an ego graph from the networkx graph
+        ego = egoGraph(1, G)
+
+        # Check the node attribute
+        assert ego.node == 1
+        #
+        print(type(ego.nxGraph))
+        # Check the graph attribute
+        assert isinstance(ego.nxGraph, nx.Graph)
+
+class TestGetNeighbors:
+    """
+    Test the getNeighbors method of the egoGraph class.
+    """
+    def test_getNeighbors(self, G):
+        # Create an ego graph from the networkx graph
+        ego = egoGraph(1, G)
+
+        # Compute the neighbors
+        result = ego.getNeighbors()
+
+        # Check the neighbors
+        assert result["t1_neighbors"] == [2, 6, 12]
+        assert result["t2_neighbors"] == [3,5,7,11]
+
+
+class TestGetIntersction:
+    def test_getIntersection(self, G):
+        # Create two ego graphs from the networkx graph
+        ego1 = egoGraph(1, G)
+        ego2 = egoGraph(2, G)
+
+        # Compute the intersection
+        result = ego1.getIntersection(ego2)
+
+        # Check the intersection set
+        assert set(result["intersection"]) == {1,2,3,6,7,12}
+
+        # Check the proportions of paths of different lengths
+        assert pytest.approx(result["len1_prop"], 0.01) == 0.333
+        assert pytest.approx(result["len2_prop"], 0.01) == 0.0
+        assert pytest.approx(result["len3_prop"], 0.01) == 0.666
+        assert pytest.approx(result["len4_prop"], 0.01) == 0.0
+
+class TestGetGraphJSON:
+    def test_getGraphJSON(self, G):
+        # Create an ego graph from the networkx graph
+        ego = egoGraph(1, G)
+
+        # Compute the graph JSON
+        result = ego.getGraphJSON()
+        print(result)
+        # Check the graph JSON
+        assert result == "{\"directed\": false, \"multigraph\": false, \"graph\": {}, \"nodes\": [{\"id\": 1}, {\"id\": 2}, {\"id\": 3}, {\"id\": 5}, {\"id\": 6}, {\"id\": 7}, {\"id\": 11}, {\"id\": 12}], \"edges\": [{\"from\": 1, \"to\": 2}, {\"from\": 1, \"to\": 6}, {\"from\": 1, \"to\": 12}, {\"from\": 2, \"to\": 3}, {\"from\": 2, \"to\": 7}, {\"from\": 5, \"to\": 6}, {\"from\": 6, \"to\": 11}]}"
+
+if __name__ == '__main__':
+    unittest.main()
