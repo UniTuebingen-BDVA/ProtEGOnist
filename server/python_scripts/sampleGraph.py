53c8ae66f6447c1adb171e6262e64bf68b14ca49
NiBrc <26893899+NiBrc@users.noreply.github.com>
2023-07-19 11:52:48 +0200

    migrate radarchar to random string picks

--- server/python_scripts/sampleGraph.py
+++ server/python_scripts/sampleGraph.py
@@ -5,12 +5,12 @@ from server.python_scripts.egoGraph import Intersection
 
 
 # generate a dorogovtsev_goltsev_mendes_graph for the test data
-def generate_test_graph_data():
+def generate_dgm_radar_data():
     G = nx.dorogovtsev_goltsev_mendes_graph(7)
     # add attributes to nodes with the name of the node being the id
     for node in G.nodes:
         G.nodes[node]["name"] = str(node)
-    random.seed(32)
+    random.seed(31)
     # add a class attribute randomly out of ['A','B', 'C', 'D', 'E']  to each node
     for node in G.nodes:
         G.nodes[node]["classification"] = random.choice(["A", "B", "C", "D", "E"])
@@ -18,7 +18,7 @@ def generate_test_graph_data():
     # print amount of nodes
     # print("Graph nodes: ", G.number_of_nodes())
     # generate 40 unique random numbers between 0 and 9842 with fixed seed
-    random.seed(32)
+    random.seed(31)
 
     random_numbers = random.sample(range(0, 1093), 40)
 
@@ -26,26 +26,50 @@ def generate_test_graph_data():
     return random_numbers, ego_networks
 
 
-def generate_random_ego_graph():
+def generate_radar_data(string_graph: nx.Graph, target_node: str):
+    random.seed(31)
+    for node in string_graph.nodes:
+        string_graph.nodes[node]["name"] = str(
+            node
+        )  # TODO: this is a side effect, we should think on how to handle the problem of the name attribute
+        string_graph.nodes[node]["classification"] = random.choice(
+            ["A", "B", "C", "D", "E"]
+        )  # TODO same here
+
+    tar_ego_graph = EgoGraph.from_string_network(target_node, string_graph)
+
+    # generate 40 other ego networks from the string graph as a dictionary
+    # with the center node as the key
+
+    # draw 40 random names from the string graph
+    random_nodes = random.sample(list(string_graph.nodes()), 40)
+
+    # generate the networks
+    ego_networks = {
+        i: EgoGraph.from_string_network(i, string_graph) for i in random_nodes
+    }
+    return tar_ego_graph, ego_networks
+
+
+def generate_dgm_ego_graph():
     G = nx.dorogovtsev_goltsev_mendes_graph(7)
     # add attributes to nodes with the name of the node being the id
     for node in G.nodes:
         G.nodes[node]["name"] = str(node)
-    random.seed(32)
+    random.seed(31)
     # add a class attribute randomly out of ['A','B', 'C', 'D', 'E']  to each node
     for node in G.nodes:
         G.nodes[node]["classification"] = random.choice(["A", "B", "C", "D", "E"])
-    random.seed(32)
+    random.seed(31)
 
     random_number = random.randint(0, 1093)
     return EgoGraph.from_string_network(random_number, G).get_graph_JSON()
 
 
-def generate_random_ego_graph_string(stringGraph: nx.Graph):
+def generate_random_ego_graph_string(string_graph: nx.Graph, target_node: str):
     # get a random node from the stringGraph
-    random.seed(31)
-    random_node = random.choice(list(stringGraph.nodes))
-    return EgoGraph.from_string_network(random_node, stringGraph).get_graph_JSON()
+
+    return EgoGraph.from_string_network(target_node, string_graph).get_graph_JSON()
 
 
 def generate_string_intersections(ego_dicts: dict[str, EgoGraph], tar_node: str):
