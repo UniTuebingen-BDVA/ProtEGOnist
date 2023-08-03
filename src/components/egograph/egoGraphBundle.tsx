b40d13270ae99987141b3c8552bca79299c9d4f2
ThHarbig <theresa_ha@hotmail.de>
2023-08-03 12:55:18 +0200

    Thharbig/egograph features (#47)
    * Introduce pseudonodes for layer 1 nodes in egographs
    
    * improve highlighting in egograph and introduce pseudonodes

--- src/components/egograph/egoGraphBundle.tsx
+++ src/components/egograph/egoGraphBundle.tsx
@@ -3,47 +3,69 @@ import { useAtom } from 'jotai';
 
 import {
     colorScaleAtom,
-    egoGraphBundleAtom, innerRadiusAtom,
+    egoGraphBundleAtom,
+    highlightedNodIndicesAtom,
+    innerRadiusAtom,
     nodeRadiusAtom,
-    nodesAtomsAtom, outerRadiusAtom
+    nodesAtomsAtom,
+    outerRadiusAtom
 } from './egoGraphBundleStore';
 import { EgographNode } from './egographNode';
 
-const EgographBundle = (props:{x:number,y:number}) => {
-    const {x,y}=props
+const EgographBundle = (props: { x: number; y: number }) => {
+    const { x, y } = props;
     const [layout] = useAtom(egoGraphBundleAtom);
     const [nodeAtoms] = useAtom(nodesAtomsAtom);
     const [colorScale] = useAtom(colorScaleAtom);
     const [nodeRadius] = useAtom(nodeRadiusAtom);
-    const [innerRadius]=useAtom(innerRadiusAtom);
-    const [outerRadius]=useAtom(outerRadiusAtom);
+    const [innerRadius] = useAtom(innerRadiusAtom);
+    const [outerRadius] = useAtom(outerRadiusAtom);
+    const [highlightedNodeIndices] = useAtom(highlightedNodIndicesAtom);
 
     return useMemo(() => {
         let lines = [];
-        const layoutCircles = layout.centers.map((center,i) => {
+        const foregroundBands: React.ReactElement[] = [];
+        const backgroundBands: React.ReactElement[] = [];
+        const layoutCircles = layout.centers.map((center, i) => {
             return (
                 <g key={i}>
-                    <circle cx={center.x} cy={center.y} r={innerRadius} stroke={"lightgray"} fill={"none"}/>
-                    <circle cx={center.x} cy={center.y} r={outerRadius} stroke={"lightgray"} fill={"none"}/>
+                    <circle
+                        cx={center.x}
+                        cy={center.y}
+                        r={innerRadius}
+                        stroke={'lightgray'}
+                        fill={'none'}
+                    />
+                    <circle
+                        cx={center.x}
+                        cy={center.y}
+                        r={outerRadius}
+                        stroke={'lightgray'}
+                        fill={'none'}
+                    />
                 </g>
             );
         });
-        const circles = Object.values(layout.nodes).map((node, i) => {
-            return (
-                <EgographNode
-                    key={node.id}
-                    centerPoint={{ x: node.cx, y: node.cy }}
-                    nodeRadius={nodeRadius}
-                    nodeAtom={nodeAtoms[i]}
-                    fill={String(colorScale(node.numEdges))}
-                />
-            );
+        const circles: React.ReactElement[] = [];
+        Object.values(layout.nodes).forEach((node, i) => {
+            if (!node.pseudo) {
+                circles.push(
+                    <EgographNode
+                        key={node.id}
+                        centerPoint={{ x: node.cx, y: node.cy }}
+                        nodeRadius={nodeRadius}
+                        nodeAtom={nodeAtoms[i]}
+                        fill={String(colorScale(node.numEdges))}
+                    />
+                );
+            }
         });
 
         lines = layout.edges.map((edge) => {
+            // show edge if any node with the same original ID as source/target is hovered
             const isVisible =
-                layout.nodes[edge.sourceIndex].hovered ||
-                layout.nodes[edge.targetIndex].hovered;
+                highlightedNodeIndices.includes(edge.sourceIndex) ||
+                highlightedNodeIndices.includes(edge.targetIndex);
             return (
                 <line
                     key={String(edge.source) + String(edge.target)}
@@ -55,9 +77,24 @@ const EgographBundle = (props:{x:number,y:number}) => {
                 />
             );
         });
-        lines.push(
-            ...layout.identityEdges.map((edge) => {
-                return (
+        layout.identityEdges.forEach((edge) => {
+            const isHighlighted =
+                highlightedNodeIndices.includes(edge.sourceIndex) ||
+                highlightedNodeIndices.includes(edge.targetIndex);
+            if (isHighlighted) {
+                foregroundBands.push(
+                    <line
+                        key={edge.id}
+                        x1={edge.x1}
+                        x2={edge.x2}
+                        y1={edge.y1}
+                        y2={edge.y2}
+                        stroke={'black'}
+                        strokeWidth={nodeRadius * 2}
+                    />
+                );
+            } else {
+                backgroundBands.push(
                     <line
                         key={edge.id}
                         x1={edge.x1}
@@ -69,15 +106,30 @@ const EgographBundle = (props:{x:number,y:number}) => {
                         strokeWidth={nodeRadius * 2}
                     />
                 );
-            })
-        );
+            }
+        });
         return (
             <g transform={`translate(${x},${y})`}>
                 {layoutCircles}
                 {lines}
+                {backgroundBands}
+                {foregroundBands}
                 {circles}
             </g>
         );
-    }, [colorScale, innerRadius, layout.centers, layout.edges, layout.identityEdges, layout.nodes, nodeAtoms, nodeRadius, outerRadius, x, y]);
+    }, [
+        colorScale,
+        highlightedNodeIndices,
+        innerRadius,
+        layout.centers,
+        layout.edges,
+        layout.identityEdges,
+        layout.nodes,
+        nodeAtoms,
+        nodeRadius,
+        outerRadius,
+        x,
+        y
+    ]);
 };
 export default EgographBundle;
