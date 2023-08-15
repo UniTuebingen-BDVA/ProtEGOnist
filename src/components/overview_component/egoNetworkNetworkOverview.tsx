fb4c305c0d59a9aa0608fb834d0d36d6df80c3fb
NiBrc <26893899+NiBrc@users.noreply.github.com>
2023-08-15 15:52:50 +0200

    overviewNode hovering

--- src/components/overview_component/egoNetworkNetworkOverview.tsx
+++ src/components/overview_component/egoNetworkNetworkOverview.tsx
@@ -1,25 +1,30 @@
 import { useAtom } from 'jotai';
-import { aggregateNetworkAtom, scaleNodeSizeAtom } from './egoNetworkNetworkOverviewStore';
+import {
+    aggregateNetworkAtom,
+    scaleNodeSizeAtom
+} from './egoNetworkNetworkOverviewStore';
 import EgoNetworkNetworkOverviewNode from './egoNetworkNetworkOverviewNode';
 import EgoNetworkNetworkOverviewEdge from './egoNetworkNetworkOverviewEdge';
-import { accountedProteinsNeigborhoodAtom} from "../../apiCalls.ts";
+import { accountedProteinsNeigborhoodAtom } from '../../apiCalls.ts';
 import { selectedProteinsAtom } from '../selectionTable/tableStore';
 import * as d3 from 'd3';
 import { tarNodeAtom } from '../radarchart/radarStore.ts';
 const EgoNetworkNetworkOverview = () => {
     const [{ nodes, edges }] = useAtom(aggregateNetworkAtom);
-    const [selectedEgoCenters] = useAtom(selectedProteinsAtom)
-    const [accountedProteinsNeigborhood] = useAtom(accountedProteinsNeigborhoodAtom)
-    const [scaleSize] = useAtom(scaleNodeSizeAtom)
-        const [tarNode] = useAtom(tarNodeAtom);
+    const [selectedEgoCenters] = useAtom(selectedProteinsAtom);
+    const [accountedProteinsNeigborhood] = useAtom(
+        accountedProteinsNeigborhoodAtom
+    );
+    const [scaleSize] = useAtom(scaleNodeSizeAtom);
+    const [tarNode] = useAtom(tarNodeAtom);
     return (
         <g>
-            {edges.map( edge => {
+            {edges.map((edge) => {
                 return (
                     <EgoNetworkNetworkOverviewEdge
                         key={edge.source.id + '+' + edge.target.id}
-                        source={edge.source}
-                        target={edge.target}
+                        source={edge.source.id}
+                        target={edge.target.id}
                         weight={edge.weight}
                         x1={edge.source.x}
                         y1={edge.source.y}
@@ -29,27 +34,38 @@ const EgoNetworkNetworkOverview = () => {
                 );
             })}
 
-            {nodes.map( (node) => {
-                let sizeNode = Math.sqrt(scaleSize(node.size)/Math.PI)
-                let nodeNeighbors = node.neighbors ?? []
-                let setProteinSelected = new Set(selectedEgoCenters)
-                
+            {nodes.map((node) => {
+                let sizeNode = Math.sqrt(scaleSize(node.size) / Math.PI);
+                let nodeNeighbors = node.neighbors ?? [];
+                let setProteinSelected = new Set(selectedEgoCenters);
+
                 // Intersection between nodeNeighbors and accountedProteinsNeigborhood
-                let coverageProteins = nodeNeighbors.filter(value => accountedProteinsNeigborhood.has(value)).length / nodeNeighbors.length ?? 0;
-                let isProteinSelected = setProteinSelected.has(node.id)
-                let colorGradientFill = d3.scaleLinear().domain([0, 1]).range(["white", '#1f78b4']);
-                    return (
-                        <EgoNetworkNetworkOverviewNode
-                            key={node.id}
-                            id={node.id}
-                            size={sizeNode}
-                            x={node.x}
-                            y={node.y}
-                            color={tarNode===node.id?'#ffff99':isProteinSelected?'#ff7f00':colorGradientFill(coverageProteins)}
-                        />
-                    );
+                let coverageProteins =
+                    nodeNeighbors.filter((value) =>
+                        accountedProteinsNeigborhood.has(value)
+                    ).length / nodeNeighbors.length ?? 0;
+                let isProteinSelected = setProteinSelected.has(node.id);
+                let colorGradientFill = d3
+                    .scaleLinear()
+                    .domain([0, 1])
+                    .range(['white', '#1f78b4']);
+                return (
+                    <EgoNetworkNetworkOverviewNode
+                        key={node.id}
+                        id={node.id}
+                        size={sizeNode}
+                        x={node.x}
+                        y={node.y}
+                        color={
+                            tarNode === node.id
+                                ? '#ffff99'
+                                : isProteinSelected
+                                ? '#ff7f00'
+                                : colorGradientFill(coverageProteins)
+                        }
+                    />
+                );
             })}
-             
         </g>
     );
 };
--- src/components/overview_component/egoNetworkNetworkOverviewEdge.tsx
+++ src/components/overview_component/egoNetworkNetworkOverviewEdge.tsx
@@ -1,6 +1,10 @@
+import { useAtom } from 'jotai';
+import { highlightNodeAtom } from './egoNetworkNetworkOverviewStore';
+
 interface EgoNetworkNetworkEdgeProps {
     weight: number;
-    opacity: number;
+    source: string;
+    target: string;
     x1: number;
     y1: number;
     x2: number;
@@ -8,15 +12,23 @@ interface EgoNetworkNetworkEdgeProps {
 }
 
 const EgoNetworkNetworkEdge = (props: EgoNetworkNetworkEdgeProps) => {
-    const { weight, x1, x2, y1, y2 } = props;
+    const { weight, x1, x2, y1, y2, source, target } = props;
+    const [highlightNode] = useAtom(highlightNodeAtom);
+
+    const color =
+        highlightNode === source || highlightNode === target
+            ? '#ff0000'
+            : '#000000';
+    const opacity =
+        highlightNode === source || highlightNode === target ? 1 : weight;
     return (
         <line
-            opacity={weight}
+            opacity={opacity}
             x1={x1}
             y1={y1}
             x2={x2}
             y2={y2}
-            stroke="black"
+            stroke={color}
             strokeWidth={weight * 10}
         />
     );
