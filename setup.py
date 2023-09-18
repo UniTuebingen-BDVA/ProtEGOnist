90f541fe3a0ac27ac9450bbba4cb6950d325759d
ThHarbig <theresa_ha@hotmail.de>
2023-09-18 14:28:12 +0200

    add pythonscripts to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='server',
-    packages=['server'],
+    packages=['server','python_scripts'],
     include_package_data=True,
     install_requires=[
         'flask',
