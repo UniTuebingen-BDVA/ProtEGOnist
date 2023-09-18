24e54745932a86f0188f7809667efefd306f419b
ThHarbig <theresa_ha@hotmail.de>
2023-09-18 14:38:18 +0200

    use find_packages()

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='server',
-    packages=['server','python_scripts'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
