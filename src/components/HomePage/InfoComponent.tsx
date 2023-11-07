cc018d3d7e9b1da1d598f4a9a11fd2808c7fd3fc
ThHarbig <theresa_ha@hotmail.de>
2023-11-07 17:25:08 +0100

    add info descriptions

--- src/components/HomePage/InfoComponent.tsx
+++ src/components/HomePage/InfoComponent.tsx
@@ -15,7 +15,7 @@ export const infoContentAtom = atom('');
 
 const titles = {
     '': '',
-    egoNetworkNetwork: 'Ego Network Network',
+    egoNetworkNetwork: 'Ego-graph Subnetwork',
     radarChart: 'Radar Chart',
     networkOverview: 'Network Overview',
     dataTable: 'Data Table',
@@ -27,57 +27,80 @@ const contents = {
     egoNetworkNetwork: (
         <div>
             <p>
-                The Ego Network Network is a visualization of the ego networks
-                of the entity of interest. The ego networks are visualized as
-                networks, where the entity of interest are the center nodes. The
-                entity that are connected to the entity of interest are the
-                first neighbors, the entity that are connected to the first
-                neighbors are the second neighbors, and so on. The entity of
-                interest are the center nodes of the ego networks.
+                Each node in the ego-graph subnetwork corresponds to an
+                ego-graph, i.e. the node and its respective neighbors of
+                distance 1 and 2. The links connecting the nodes show the
+                overlap of the respective ego-graph neighborhoods.
             </p>
             <p>
-                The entity of interest are colored according to the color legend
-                on the right side. The entity that are connected to the entity
-                of interest are colored according to the color legend on the
-                left side. The entity that are connected to the first neighbors
-                are colored according to the color legend on the left side.
+                <b>Click on</b> nodes or links to decollapse ego-graphs to show the
+                detailed internal structure including all contained elements and
+                their interactions. Similarly, collapse nodes by clicking on the
+                ego-graphs.
+            </p>
+            <p>
+                Groups of up to three connected ego-graphs can be decollapsed
+                for a direct comparison. Within the groups bands show which
+                elements are shared between the ego-graphs.
+            </p>
+            <p>
+                <b>Click on</b> bands in groups of decollapsed ego-graphs to select
+                them for detailed investigation using the selection table.
             </p>
         </div>
     ),
     radarChart: (
         <div>
             <p>
-                The Radar Chart is a visualization of the intersection of the
-                ego networks of the entity of interest. The ego networks are
-                visualized as networks, where the entity of interest are the
-                center nodes.
+                The radar chart visualizes an ego-graph of interest and the its
+                similarity to other ego-graphs. The 30 most similar
+                ego-graphs are displayed.
             </p>
+            <p><b>Click on</b> ego-graphs to change the center.</p>
         </div>
     ),
     networkOverview: (
         <div>
             <p>
-                The Network Overview is a visualization of the intersection of
-                the ego networks of the entity of interest. The ego networks are
-                visualized as networks, where the entity of interest are the
-                center nodes.
+                The Network Overview contains potentially interesting
+                ego-graphs. Elements from the input data set are chosen such
+                that the highest abount of elements and interactions are
+                contained in ego-graphs generated from the chosen elements.
+            </p>
+            <p>
+                <b>Click on</b> ego-graphs (nodes) to add them to the ego-graph
+                subnetwork and set them as center of the radar chart.
             </p>
         </div>
     ),
     dataTable: (
         <div>
+            <p>The Data Table contains information about all elements.</p>
             <p>
-                The Data Table is a table that contains the information about
-                the entity of interest.
+                <b>Use the checkboxes</b> to add elements to the ego-graph subnetwork
+                or <b>click on the radar symbol</b> to place them as the center in the
+                radar chart.
             </p>
         </div>
     ),
     protegonist: (
         <div>
             <p>
-                ProtEGOnist is a new visualization tool that allows users to
-                explore large networks by using an approach based on ego
-                networks.
+                The complexity of protein-protein interaction (PPI) networks
+                often leads to visual clutter and limited interpretability. To
+                overcome these problems, we present ProtEGOnist, a novel
+                visualization tool designed to explore PPI networks with a focus
+                on drug-protein associations. ProtEGOnist addresses the
+                challenges using ego-graphs that represent local PPI
+                neighborhoods around proteins of interest. These ego-graphs are
+                arranged in an ego-graph network, where edges between ego-graphs
+                encode their similarity using the Jaccard index. The tool offers
+                an overview of drug-associated proteins, a radar chart to
+                compare protein functions, and detailed ego-graph subnetworks
+                for interactive exploration. Our design aims to reduce visual
+                complexity while enabling detailed exploration, and facilitating
+                the discovery of meaningful patterns in PPI networks. The ProtEGOnist
+                approach can be applied to other data sets, e.g. social networks.
             </p>
         </div>
     )
