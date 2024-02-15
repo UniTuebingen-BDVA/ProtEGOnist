f8a16162ea1edf380c134b51cd3e9a1f69789e51
NiBrc <26893899+NiBrc@users.noreply.github.com>
2024-02-15 16:50:41 +0100

    generate ego_graph related metadata

--- server/python_scripts/utilities_preprocessing/generate_ego_graphs.py
+++ server/python_scripts/utilities_preprocessing/generate_ego_graphs.py
@@ -1,8 +1,7 @@
-
-
 from networkx import read_weighted_edgelist, read_graphml, Graph
 from egoGraph import EgoGraph
 import numpy as np
+import pandas as pd
 import argparse
 
 
@@ -33,7 +32,7 @@ def create_ego_graph(network: Graph, node: str) -> dict:
     return ego_dict
 
 
-def create_ego_graphs_for_network(network: Graph) -> dict:
+def create_ego_graphs_dicts_for_network(network: Graph) -> dict:
     """
     Create an ego graph for each node in a networkX element.
     """
@@ -43,10 +42,46 @@ def create_ego_graphs_for_network(network: Graph) -> dict:
     return ego_graphs
 
 
+def create_ego_graphs_for_network(network: Graph) -> dict:
+    """
+    Create an ego graph for each node in a networkX element.
+    """
+    ego_graphs = {}
+    for node in network.nodes:
+        ego_graphs[node] = EgoGraph.from_string_network(node, network)
+    return ego_graphs
+
+
+def ego_graphs_to_metadata(ego_graphs: dict, metadata: pd.DataFrame) -> pd.DataFrame:
+    """
+    add metadata dervied from ego graphs to a metadata dat
+    metadata to be added includes:
+    - number of nodes
+    - number of edges
+    - average degree
+    - level 1 neighbours
+    - level 2 neighbours
+    """
+    for node in ego_graphs:
+        ego_graph = ego_graphs[node]
+        ego_graph_neighbors = ego_graph.get_neighbors()
+        degree_1_alters = ego_graph_neighbors["t1_neighbors"]
+        degree_2_alters = ego_graph_neighbors["t2_neighbors"]
+        all_nodes = degree_1_alters + degree_2_alters + [node]
+        all_edges = ego_graph.get_edge_set()
+        # current row index in metadata
+        print(metadata.index)
+        metadata.loc[node, "number_of_nodes"] = len(all_nodes)
+        metadata.loc[node, "number_of_edges"] = len(all_edges)
+        metadata.loc[node, "average_degree"] = len(all_edges) / len(all_nodes)
+        metadata.loc[node, "degree_1_alters"] = len(degree_1_alters)
+        metadata.loc[node, "degree_2_alters"] = len(degree_2_alters)
+    return metadata
+
+
 def main():
     # create parser
-    parser = argparse.ArgumentParser(
-        description="Create ego graphs from a network.")
+    parser = argparse.ArgumentParser(description="Create ego graphs from a network.")
     # add arguments to the parser
     parser.add_argument(
         "-i",
@@ -62,6 +97,27 @@ def main():
         help="Path to the output pickle file.",
         default="ego_dict_as_pickle.npy",
     )
+    parser.add_argument(
+        "-m",
+        "--metadata",
+        type=str,
+        help="Path to the metadata file.",
+        default="metadata.csv",
+    )
+    parser.add_argument(
+        "-mo",
+        "--metadata_output",
+        type=str,
+        help="Path to the output metadata file.",
+        default="metadata_new.csv",
+    )
+    parser.add_argument(
+        "-s",
+        "--separator",
+        type=str,
+        help="Separator for the metadata file.",
+        default=";",
+    )
     # parse the arguments
     args = parser.parse_args()
     # read the network
@@ -74,7 +130,23 @@ def main():
     ego_graphs = create_ego_graphs_for_network(network)
     print(len(ego_graphs.keys()))
     # save the ego graphs as pickle file
-    np.save(args.output, ego_graphs)
+    # np.save(args.output, ego_graphs)
+    # read the metadata
+    metadata = pd.read_csv(
+        args.metadata,
+        index_col=0,
+        sep=args.separator,
+        encoding="utf-8",
+    )
+    print(metadata)
+    # add metadata to the metadata dataframe
+    metadata = ego_graphs_to_metadata(ego_graphs, metadata)
+    # save the metadata
+    metadata.to_csv(
+        args.metadata_output,
+        sep=args.separator,
+        encoding="utf-8",
+    )
 
 
 if __name__ == "__main__":
